c9e05409e13bfebbcdd4e38988ee8a96b9233b84
Matt Beall <mbeall@starverte.com>
2017-02-24 14:07:11 -0700

    Beautify functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,27 @@
 <?php
-function surveyor_language_setup() {
-  load_child_theme_textdomain('uncode', get_stylesheet_directory() . '/languages');
-}
-add_action('after_setup_theme', 'surveyor_language_setup');
+/**
+ * Surveyor functions and definitions
+ *
+ * @package Uncode\Surveyor
+ */
 
-function theme_enqueue_styles() {
-  $production_mode = ot_get_option('_uncode_production');
-  $resources_version = ($production_mode === 'on') ? null : rand();
-  $parent_style = 'uncode-style';
-  $child_style = array('uncode-custom-style');
-  wp_enqueue_style($parent_style, get_template_directory_uri() . '/library/css/style.css', array(), $resources_version);
-  wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', $child_style, $resources_version);
+/**
+ * Load Surveyor's translated strings.
+ */
+function surveyor_language_setup() {
+  load_child_theme_textdomain( 'uncode', get_stylesheet_directory() . '/languages' );
 }
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+add_action( 'after_setup_theme', 'surveyor_language_setup' );
+
+/**
+ * Enqueue scripts and styles
+ */
+function theme_enqueue_styles() {
+  $production_mode = ot_get_option( '_uncode_production' );
+  $resources_version = ( 'on' === $production_mode ) ? null : rand();
+  $parent_style = 'uncode-style';
+  $child_style = array( 'uncode-custom-style' );
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/library/css/style.css', array(), $resources_version );
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', $child_style, $resources_version );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
